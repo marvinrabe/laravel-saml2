36dfb26db84107ff1f8d584e02f24997c7fc37ae
Marvin Rabe <marvin.rabe@gmail.com>
2021-04-26 15:27:08 +0200

    fix: remove blacklist to keep things simple.

--- src/Listeners/LoginListener.php
+++ src/Listeners/LoginListener.php
@@ -2,7 +2,6 @@
 
 namespace Aacotroneo\Saml2\Listeners;
 
-use Aacotroneo\Saml2\Blacklist;
 use Aacotroneo\Saml2\Events\Login;
 use Aacotroneo\Saml2\User;
 use Illuminate\Support\Facades\Auth;
@@ -10,27 +9,10 @@ use Illuminate\Support\Facades\Log;
 
 abstract class LoginListener
 {
-    protected $blacklist;
-
-    public function __construct(Blacklist $blacklist)
-    {
-        $this->blacklist = $blacklist;
-    }
-
     abstract protected function findUser(User $user);
 
     public function handle(Login $event)
     {
-        $messageId = $event->getAuth()->getLastMessageId();
-
-        if ($this->blacklist->has($messageId)) {
-            Log::info('SSO failed: Message ID blacklisted', ['messageId' => $messageId]);
-
-            return;
-        }
-
-        $this->blacklist->add($messageId);
-
         Log::info('SSO: ' . $event->getUser()->getUserId(), ['attributes' => $event->getUser()->getAttributes()]);
 
         Auth::login($this->findUser($event->getUser()));
